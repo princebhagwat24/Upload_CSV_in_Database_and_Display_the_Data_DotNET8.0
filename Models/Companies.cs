e34023b96aa416901f8e867356dc64b077039dcf
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 13:56:23 +0530

    Add files via upload

---
+++ Models/Companies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CsvToDatabaseApi.Models
+{
+    public class Companies
+    {
+        public string OrganizationId { get; set; }
+        public string Name { get; set; }
+        public string Website { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public int Founded { get; set; }
+        public string Industry { get; set; }
+        public int NumberofEmployees { get; set; }
+    }
+}
